3f51c254a885578168456666f72c9abf77af8688
Sacha Brisset <sacha.brisset@hotmail.fr>
2018-03-01 07:22:38 +0100

    Added the keyword file to the project

--- AutoBraces/TestSql.sql
+++ AutoBraces/TestSql.sql
@@ -1,9 +1,15 @@
-USE AdventureWorks2012;
-GO
-SELECT 'Total income is', ((OrderQty * UnitPrice) * (1.0 - UnitPriceDiscount)), ' for ',
-p.Name AS ProductName 
-FROM Production.Product AS p 
-INNER JOIN Sales.SalesOrderDetail AS sod
-ON p.ProductID = sod.ProductID 
-ORDER BY ProductName ASC;
-GO
+with dups as  (
+SELECT 
+   COUNT(CARD_ID)  OVER (PARTITION BY cade_id, leve_id) cardCount
+   cade_id,
+   leve_id
+FROM  carddetails 
+)
+    SELECT * 
+    FROM   cards c 
+           INNER JOIN carddetails cd 
+             ON c.card_id = cd.card_id 
+           INNER JOIN  dups 
+             ON cd.cade_id = dups.cade_id 
+                AND cd.leve_id = dups.leve_id 
+    WHERE cardCount > 1
