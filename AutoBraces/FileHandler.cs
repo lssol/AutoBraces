8047ab4b7b95a9808e4046598f25c49325a8c52b
Sacha Brisset <sacha.brisset@hotmail.fr>
2018-03-01 07:00:41 +0100

    Added unversioned files

---
+++ AutoBraces/FileHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace AutoBraces
+{
+    public class FileHandler
+    {
+        public static void CreateNewFile(string originalPath, string content)
+        {
+            var directory = Path.GetDirectoryName(originalPath);
+            var fileName = Path.GetFileNameWithoutExtension(originalPath);
+            var newPath = Path.Combine(directory, fileName + "_quoted.sql");
+
+            if (File.Exists(newPath))
+            {
+                Console.WriteLine("The file already exists");
+                return;
+            }
+            File.WriteAllText(newPath, content);
+        }
+    }
+}
